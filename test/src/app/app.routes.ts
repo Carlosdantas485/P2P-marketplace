f3be4d4c95a32fe9b704af3a292acc9e815cc577
CarlosDantas <60883563+Carlosdantas485@users.noreply.github.com>
2025-06-12 11:13:50 -0300

    remove account component

--- test/src/app/app.routes.ts
+++ test/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
-import { AccountComponent } from './components/account/account.component';
 import { InventoryComponent } from './components/inventory/inventory.component';
 
 export const routes: Routes = [
@@ -10,7 +9,6 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'account', component: AccountComponent },
   { path: 'inventory', component: InventoryComponent },
   { path: '**', redirectTo: '/login' }
 ];
