fa646a8788a54e4250acfca4c7dadc652698b676
CarlosDantas <60883563+Carlosdantas485@users.noreply.github.com>
2025-06-12 15:00:59 -0300

    transfer history filter implemented

--- test/src/app/components/history/history.component.ts
+++ test/src/app/components/history/history.component.ts
@@ -11,6 +11,13 @@ import { HistoryService } from '../../services/history.service';
 })
 export class HistoryComponent implements OnInit {
   history: any[] = [];
+  filteredHistory: any[] = [];
+  filterType: string = 'all';
+
+  totalCompras: number = 0;
+  totalVendas: number = 0;
+  totalDepositos: number = 0;
+
   loading = true;
   error: string | null = null;
 
@@ -19,7 +26,9 @@ export class HistoryComponent implements OnInit {
   ngOnInit(): void {
     this.historyService.getHistory().subscribe(
       (data) => {
-        this.history = data;
+        this.history = data.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+        this.calculateTotals();
+        this.applyFilter('all');
         this.loading = false;
       },
       (err) => {
@@ -28,5 +37,30 @@ export class HistoryComponent implements OnInit {
         console.error(err);
       }
     );
+  }
+
+  calculateTotals(): void {
+    this.totalCompras = 0;
+    this.totalVendas = 0;
+    this.totalDepositos = 0;
+
+    for (const item of this.history) {
+      if (item.type === 'compra') {
+        this.totalCompras += item.price;
+      } else if (item.type === 'venda') {
+        this.totalVendas += item.price;
+      } else if (item.type === 'deposito') {
+        this.totalDepositos += item.amount;
+      }
+    }
+  }
+
+  applyFilter(type: string): void {
+    this.filterType = type;
+    if (type === 'all') {
+      this.filteredHistory = this.history;
+    } else {
+      this.filteredHistory = this.history.filter(item => item.type === type);
+    }
   }
 }
